370bcad4acbefbd555ab12362f009d960603d18e
AdityaK-256 <106029974+AdityaK-256@users.noreply.github.com>
2025-08-01 02:29:52 +0530

    practicing insertion sort

--- insertion.java
+++ insertion.java
@@ -1,6 +1,6 @@
 class insertion{
     public static void main(String [] args){
-        int ar[] = {7,6,3,5,2,4,1};
+        int ar[] = {7,6,3,5,2,4,1};// {6,3,5,7,2,4,1};
         for(int x = 1; x<ar.length;x++){
             int y= x-1;
             int current = ar[x];
